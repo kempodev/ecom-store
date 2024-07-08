bfe654a7a36fac9e0b80ac17b18af2d4399c50d4
Joona Kemppainen <joona.kemppainen@pm.me>
2024-07-08 18:00:19 -0400

    comment out unfinished files

--- components/BillboardCarousel.tsx
+++ components/BillboardCarousel.tsx
@@ -1,42 +1,43 @@
-'use client'
+// 'use client'
 
-import * as React from 'react'
+// import * as React from 'react'
 
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from '@/components/ui/carousel'
-import Autoplay from 'embla-carousel-autoplay'
+// import {
+//   Carousel,
+//   CarouselContent,
+//   CarouselItem,
+//   // CarouselNext,
+//   // CarouselPrevious,
+// } from '@/components/ui/carousel'
+// import Autoplay from 'embla-carousel-autoplay'
 
-import type { Billboard as BillboardType } from '@/types'
-import Billboard from './Billboard'
-import useEmblaCarousel from 'embla-carousel-react'
-import type { EmblaOptionsType } from 'embla-carousel'
+// import type { Billboard as BillboardType } from '@/types'
+// import Billboard from './Billboard'
+// import useEmblaCarousel from 'embla-carousel-react'
+// import type { EmblaOptionsType } from 'embla-carousel'
 
-type Props = {
-  data: BillboardType[]
-}
+// type Props = {
+//   data: BillboardType[]
+// }
 
-const OPTIONS: EmblaOptionsType = {
-  loop: true,
-}
+// const OPTIONS: EmblaOptionsType = {
+//   loop: true,
+// }
 
-export default function BillboardCarousel({ data }: Props) {
-  const [emblaRef, emblaApi] = useEmblaCarousel(OPTIONS, [Autoplay()])
-  return (
-    <Carousel className='w-full' plugins={[Autoplay()]} ref={emblaRef}>
-      <CarouselContent>
-        {data.map((billboard) => (
-          <CarouselItem key={billboard.id}>
-            <Billboard data={billboard} />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      {/* <CarouselPrevious />
-      <CarouselNext /> */}
-    </Carousel>
-  )
-}
+// export default function BillboardCarousel({ data }: Props) {
+// const [emblaRef, emblaApi] = useEmblaCarousel(OPTIONS, [Autoplay()])
+// return (
+// null
+// <Carousel className='w-full' plugins={[Autoplay()]} ref={emblaRef}>
+//   <CarouselContent>
+//     {data.map((billboard) => (
+//       <CarouselItem key={billboard.id}>
+//         <Billboard data={billboard} />
+//       </CarouselItem>
+//     ))}
+//   </CarouselContent>
+//   {/* <CarouselPrevious />
+//   <CarouselNext /> */}
+// </Carousel>
+// )
+// }
